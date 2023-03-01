4a9c978ffe885b43769046aa53687b741de6eda4
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-03-01 18:26:44 -0500

    Update get_model_with_name.py

--- chatgpt_hackathon/get_model_with_name.py
+++ chatgpt_hackathon/get_model_with_name.py
@@ -11,7 +11,7 @@ def get_model_with_name(client:lb.client, model_name:str):
     models = list(client.get_models(where=(lb.Model.name==model_name)))
     if not models:
         raise ValueError(f"No models exist with name {team_name} - please ensure your Model's Name corresponds with a team name - if your team has not created a model yet, please do so")
-    elif len(models!=1):
+    elif len(models)!=1:
         raise ValueError(f"Multiple models exist with name {team_name} - please resolve this, as each team should have a unique name and only one model each")
     model = models[0] 
     return project
