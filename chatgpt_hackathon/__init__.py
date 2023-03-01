fd4e6be2573a6d6aa0dc4c68191f1ee59cdb713e
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-03-01 18:23:09 -0500

    Update __init__.py

--- chatgpt_hackathon/__init__.py
+++ chatgpt_hackathon/__init__.py
@@ -1,4 +1,4 @@
-from .create_iteration import create_iteration
 from .get_project_with_name import get_project_with_name
-from .register_team import register_team
 from .get_model_with_name import get_model_with_name
+from .register_team import register_team
+from .create_iteration import create_iteration
