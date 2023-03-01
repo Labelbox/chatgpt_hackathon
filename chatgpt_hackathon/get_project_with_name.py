2f982ec2c22cb37316534e824802053b4a13b60e
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-03-01 18:24:26 -0500

    Update get_project_with_name.py

--- chatgpt_hackathon/get_project_with_name.py
+++ chatgpt_hackathon/get_project_with_name.py
@@ -11,7 +11,7 @@ def get_project_with_name(client:lb.client, project_name:str):
     projects = list(client.get_projects(where=(lb.Project.name==project_name)))
     if not projects:
         raise ValueError(f"No projects exist with name {team_name} - please ensure your Project's Name corresponds with a team name - if your team has not created a project yet, please do so")
-    elif len(projects!=1):
+    elif len(projects)!=1:
         raise ValueError(f"Multiple projects exist with name {team_name} - please resolve this, as each team should have a unique name and only one project each")
     project = projects[0] 
     return project
