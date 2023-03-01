71e1c1c6c2a12dee034e3dc39781e13844ee2693
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-02-28 21:19:53 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='helper',
+    version='0.0.01',
+    author='Labelbox',
+    author_email="raphael@labelbox.com",
+    description='Helper repo for ChatGPT Hackathon',
+    long_description=long_description,
+    long_description_content_type="text/markdown",    
+    url='https://github.com/Labelbox/chatgpt_hackathon',    
+    packages=setuptools.find_packages(),
+    install_requires=["labelbox[data]", "openai", "packaging"]
+)
