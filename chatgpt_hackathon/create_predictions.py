f6036a421b5b34138920646fab5f321d5420f094
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-03-01 23:52:39 -0500

    Update create_predictions.py

--- chatgpt_hackathon/create_predictions.py
+++ chatgpt_hackathon/create_predictions.py
@@ -11,17 +11,20 @@ from labelbase.ontology import get_ontology_schema_to_name_path
 
 def create_predictions(api_key, client, team_name, training_round, chatgpt_model_name):
     # Get openai key
+    print(f"Connecting with OpenAI...")
     openai_key = requests.post("https://us-central1-saleseng.cloudfunctions.net/get-openai-key", data=json.dumps({"api_key" : api_key}))
     openai_key = openai_key.content.decode()
     if "Error" in openai_key:
         raise ValueError(f"Incorrect API key - please ensure that your Labelbox API key is correct and try again")
     else:
-        openai.api_key = openai_key  
+        openai.api_key = openai_key
+    print(f"Success: Connected with OpenAI")  
     training_round = str(training_round)
     model_run = get_model_run(client, team_name, training_round)        
     print(f"Exporting labels...")
     labels = model_run.export_labels(download=True)
     print(f"Export complete - {len(labels)} labels")       
+    ontology_name_path_to_schema = get_ontology_schema_to_name_path(ontology, invert=True)
     # Create predictions
     print(f"Creating predictions and uploading to model run...")
     predictions = []
@@ -43,24 +46,55 @@ def create_predictions(api_key, client, team_name, training_round, chatgpt_model
     # Return upload results
     return err
 
-def create_prediction(openai_key, chatgpt_model_name, label):
+def get_answer_and_conf(openai_key, chatgpt_model_name, label, ontology_name_path_to_schema):
     chatgpt_dict, data_row_id = get_chatgpt_input(label)
     pred = openai.Completion.create(
         api_key = openai_key,
         model = chatgpt_model_name,
         prompt = chatgpt_dict["prompt"],
-        max_tokens = 1,
-        logprobs = 4,
-        temperature = 1
-    )           
+        max_tokens = 2,
+        logprobs = 28,
+        temperature = 0
+    )
+    name_path = None
+    # Check the answer first to see if it matches the ontology
+    answer = pred['choices'][0]["text"]
+    answer_name_path = f"emotions///{answer}"
+    # If the answer name path is not in the ontology, let's see if any name paths start with what ChatGPT generated
+    if answer_name_path not in ontology_name_path_to_schema.keys():
+        for ont_path in ontology_name_path_to_schema:
+            if ont_path.startswith(answer_name_path):
+                name_path = ont_path
+                break
+    # If the answer name path is in the ontology, let's match it to its schema id
+    else:
+        name_path = answer_name_path
+    # If we still don't have a name path, let's check our tokens
+    if not name_path:
+        token = pred['choices'][0]['logprobs']['tokens'][0]+pred['choices'][0]['logprobs']['tokens'][1]
+        token = token.replace("#","")
+        tok_name_path = f"emotions///{token}"   
+        # If the token name path is not in the ontology, let's see if any name paths start with what ChatGPT generated
+        if tok_name_path not in ontology_name_path_to_schema.keys():
+            for ont_path in ontology_name_path_to_schema:
+                if ont_path.startswith(tok_name_path):
+                    name_path = ont_path
+                    break
+      # If the token name path is in the ontology, let's match it to its schema id
+        else:
+            name_path = tok_name_path
+    if not name_path:
+        print(f"Error converting ChatGPT output to Labelbox output - please share the following error statement with your professor: {pred}")
+    schema_id = ontology_name_path_to_schema[name_path]
+    pred_answer = name_path.split("///")[1]
     radio_prediction = lb_types.ClassificationAnnotation(
         name="emotion", 
         value= lb_types.Radio(answer=lb_types.ClassificationAnswer(
-            name=f"{pred['choices'][0]['logprobs']['tokens'][0]}",
+            name=pred_answer,
             confidence= 1 + (int(pred['choices'][0]['logprobs']['token_logprobs'][0])/10)
         )))     
     label_prediction = lb_types.Label(
         data=lb_types.TextData(uid=data_row_id),
         annotations = [radio_prediction]
-    )
+    )    
     return label_prediction
