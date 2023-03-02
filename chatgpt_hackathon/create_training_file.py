ec8e52353023ae443e5394f529f6962266fec34a
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-03-01 19:04:06 -0500

    Create create_training_file.py

---
+++ chatgpt_hackathon/create_training_file.py
@@ -0,0 +1,32 @@
+from chatgpt_hackathon import get_model_with_name
+
+def create_training_file(client, team_name, training_round):
+    """ For a given training round, generates a training file to-be-passed to OpenAI and a dictionary with data row ID and input data
+    """
+    model_run = get_model_run(client, team_name, training_round)
+    labels = model_run.export_labels(download=True)
+    data_row_id_to_model_input = {}
+    my_file = open("completions.jsonl", "w")
+    for label in labels:
+        text = get_text(label)
+        chatgpt_dict = {
+            "prompt" : f"{get_text(label)}#-#-#-#-#",
+            "completion" : f"{label['Label']['classifications'][0]['answer']['value']}#####"
+        }
+        data_row_id_to_model_input[label["DataRow ID"]] = chatgpt_dict
+        as_string = json.dumps(chatgpt_dict)
+        my_file.write(f"{as_string}\n")      
+    return "completions.jsonl", data_row_id_to_model_input
+  
+def get_model_run(client, team_name, training_round):
+    """ For a given model, iterates through names to get the right model run
+    """
+    model = get_model_with_name(client, team_name)
+    found_model_run = False
+    for model_run in model.model_runs():
+        if model_run.name == str(training_round):
+            found_model_run = True
+            break
+    if not found_model_run:
+        raise ValueError(f"No model run for team name {team_name} with run name {training_round}")    
+    return model_run
