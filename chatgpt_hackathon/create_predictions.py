83f29b0b20dca0f3e62f5c09e808daabdcdce568
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-03-01 22:03:40 -0500

    Update create_predictions.py

--- chatgpt_hackathon/create_predictions.py
+++ chatgpt_hackathon/create_predictions.py
@@ -16,26 +16,11 @@ def create_predictions(api_key, client, team_name, training_round, chatgpt_model
     # Create predictions
     print(f"Creating predictions and uploading to model run...")
     predictions = []
-    for data_row_id in list(data_row_id_to_input.keys())[:2]:    
-        pred = openai.Completion.create(
-            api_key = openai_key,
-            model = chatgpt_model_name,
-            prompt = data_row_id_to_input[data_row_id]["prompt"],
-            max_tokens = 1,
-            logprobs = 4,
-            temperature = 1
-        )       
-        radio_prediction = lb_types.ClassificationAnnotation(
-            name="emotion", 
-            value= lb_types.Radio(answer=lb_types.ClassificationAnswer(
-                name=f"{pred['choices'][0]['logprobs']['tokens'][0]}",
-                confidence= 1 + (int(pred['choices'][0]['logprobs']['token_logprobs'][0])/10)
-            )))        
-        label_prediction = lb_types.Label(
-            data=lb_types.TextData(uid=data_row_id),
-            annotations = [radio_prediction]
-        )       
-        predictions.append(label_prediction)
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        futures = [executor.submit(create_prediction, openai_key, chatgpt_model_name, data_row_id_to_input[data_row_id]["prompt"]) for data_row_id in data_row_id_to_input]
+        for future in tqdm(futures, total=len(data_row_id_to_input)):
+            prediction = future.result()
+            predictions.append(prediction)
     ndjson_prediction = list(NDJsonConverter.serialize(predictions)) 
     # Upload the prediction label to the Model Run
     upload_job_prediction = model_run.add_predictions(
@@ -48,3 +33,24 @@ def create_predictions(api_key, client, team_name, training_round, chatgpt_model
     print("Errors:", upload_job_prediction.err)
     # Return upload results
     return err
+
+def create_prediction(openai_key, chatgpt_model_name, prompt):
+    pred = openai.Completion.create(
+        api_key = openai_key,
+        model = chatgpt_model_name,
+        prompt = data_row_id_to_input[data_row_id]["prompt"],
+        max_tokens = 1,
+        logprobs = 4,
+        temperature = 1
+    )           
+    radio_prediction = lb_types.ClassificationAnnotation(
+        name="emotion", 
+        value= lb_types.Radio(answer=lb_types.ClassificationAnswer(
+            name=f"{pred['choices'][0]['logprobs']['tokens'][0]}",
+            confidence= 1 + (int(pred['choices'][0]['logprobs']['token_logprobs'][0])/10)
+        )))     
+    label_prediction = lb_types.Label(
+        data=lb_types.TextData(uid=data_row_id),
+        annotations = [radio_prediction]
+    )
+    return label_prediction
