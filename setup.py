877758fbec3d60ab573eff05803d59c6f14341f9
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-03-01 23:32:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",    
     url='https://github.com/Labelbox/chatgpt_hackathon',    
     packages=setuptools.find_packages(),
-    install_requires=["labelbox[data]", "openai", "packaging"]
+    install_requires=["labelbox[data]", "openai", "packaging", "labelbase"]
 )
