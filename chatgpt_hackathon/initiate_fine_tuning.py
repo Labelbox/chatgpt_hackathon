06ae92cddef20e1079bc38a484476b3966f25591
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-03-01 22:31:57 -0500

    Update initiate_fine_tuning.py

--- chatgpt_hackathon/initiate_fine_tuning.py
+++ chatgpt_hackathon/initiate_fine_tuning.py
@@ -1,4 +1,4 @@
-from chatgpt_hackathon import get_model_run
+from chatgpt_hackathon import get_model_run, get_chatgpt_input
 import requests
 import json
 import requests
@@ -20,19 +20,14 @@ def initiate_fine_tuning(api_key, client, team_name, training_round):
     openai_file = open(openai_file_name, "w")
     print(f"Creating training file...")
     with ThreadPoolExecutor(max_workers=8) as executor:
-        futures = [executor.submit(write_chatgpt_input, label) for label in labels]
+        futures = [executor.submit(get_chatgpt_input, label) for label in labels]
         for future in tqdm(futures, total=len(labels)):
             chatgpt_dict, data_row_id = future.result()
             data_row_id_to_model_input[data_row_id] = chatgpt_dict
             as_string = json.dumps(chatgpt_dict)
             openai_file.write(f"{as_string}\n")   
     openai_file.close()
-    print(f"Success: Created OpenAI training file with name `{openai_file_name}`")   
-    lb_file_name = f"lb_training_file_{training_round}.json"
-    lb_file = open(lb_file_name, "w")
-    lb_file.write(json.dumps(data_row_id_to_model_input))
-    lb_file.close()
-    print(f"Success: Created Labelbox training file with name `{openai_file}`")   
+    print(f"Success: Created OpenAI training file with name `{openai_file_name}`")    
     print(f"Connecting with OpenAI...")
     openai_key = requests.post("https://us-central1-saleseng.cloudfunctions.net/get-openai-key", data=json.dumps({"api_key" : api_key}))
     openai_key = openai_key.content.decode()
@@ -55,14 +50,4 @@ def initiate_fine_tuning(api_key, client, team_name, training_round):
     fune_tune_job_id = fine_tune_job["id"]
     print(f'Fine-tune Job with ID `{fune_tune_job_id}` initiated')
     return fune_tune_job_id, data_row_id_to_model_input
-
-def write_chatgpt_input(label):
-    gs_url = label['Labeled Data']
-    https_url = f"https://storage.googleapis.com/{gs_url[5:]}"
-    text = requests.get(https_url).content.decode()    
-    chatgpt_dict = {
-        "prompt" : f"{text}#-#-#-#-#",
-        "completion" : f"{label['Label']['classifications'][0]['answer']['value']}#####"
-    }
-    return chatgpt_dict, label["DataRow ID"]
     
